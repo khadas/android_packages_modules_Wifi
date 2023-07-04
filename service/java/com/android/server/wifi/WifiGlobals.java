0170bd4aa0be438ee39091fc685fedb123de1bf6
Alex Zhao <zzc@rock-chips.com>
2023-07-04 10:46:11 +0800

    set mIpReachabilityDisconnectEnabled to false
    Signed-off-by: Alex Zhao <zzc@rock-chips.com>
    Change-Id: I9fe398d39266edfb62c12369bab6b18d01297469

--- service/java/com/android/server/wifi/WifiGlobals.java
+++ service/java/com/android/server/wifi/WifiGlobals.java
@@ -40,7 +40,7 @@ public class WifiGlobals {
     private final Context mContext;
 
     private final AtomicInteger mPollRssiIntervalMillis = new AtomicInteger(-1);
-    private final AtomicBoolean mIpReachabilityDisconnectEnabled = new AtomicBoolean(true);
+    private final AtomicBoolean mIpReachabilityDisconnectEnabled = new AtomicBoolean(false);
     private final AtomicBoolean mIsBluetoothConnected = new AtomicBoolean(false);
 
     // These are read from the overlay, cache them after boot up.
